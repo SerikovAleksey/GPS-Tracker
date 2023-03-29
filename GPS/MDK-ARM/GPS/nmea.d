5875fac95d146b2d994d4be625f891461e6c811f
Aleksey Serikov <serikov.ar@phystech.edu>
2023-03-29 18:11:51 +0300

    update

---
+++ GPS/MDK-ARM/GPS/nmea.d
@@ -0,0 +1,6 @@
+gps\nmea.o: ..\Core\Src\NMEA.c
+gps\nmea.o: ../Core/Inc/NMEA.h
+gps\nmea.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+gps\nmea.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+gps\nmea.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+gps\nmea.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
